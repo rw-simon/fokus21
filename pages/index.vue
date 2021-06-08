55ba9d66b1f7034a37e4f80b7685f355a4801f3b
rw-simon <simon.fischer@rechtwinklig.ch>
2021-06-08 16:25:34 +0200

    Contact / registration form

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@
 				</div>
 			</div>
 		</div>
+		<div class="anmelden">
+			<a
+				href="mailto:info@fokus-berufsbildung.ch?subject=Anmeldung%20Fokus%20Berufsbildung%202021%20:%20Lernen–%20ein%20breiter%20Horizont&body=Guten%20Tag%0D%0A%0D%0AIch%20möchte%20mich%20gerne%20für%20den%20Event%20Fokus%20Berufsbildung%202021:%20Lernen%20–%20ein%20breiter%20Horizont%20anmelden.%0D%0A%0D%0AMeine%20Kontaktangaben:%20%0D%0AName:%0D%0AStrasse%20Nr.:%0D%0APLZ%20Ort:%0D%0ATelefon:%0D%0A%0D%0AFreundliche%20Grüsse"
+				>Jetzt Anmelden<span><img class="icon" src="/icons/icon_arrow_right.svg" alt=""/></span
+			></a>
+		</div>
 	</div>
 </template>
 
@@ -29,6 +35,23 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.anmelden
+	position: fixed
+	bottom: 2rem
+	right: 2rem
+	z-index: 99999999
+	a
+		color: white
+		font-size: 2rem
+		span
+			display: inline-block
+			width: 3rem
+			text-align: right
+			padding-right: 1rem
+			transition: padding-right 200ms ease-out
+		&:hover
+			span
+				padding-right: 0
 .wrapper
 	height: 100vh
 	margin: 0
